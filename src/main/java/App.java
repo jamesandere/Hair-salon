25eae9381a6af141674cc773ed611e0b7ce8b558
jamesandere <jamesandere56@gmail.com>
2019-04-23 20:56:11 +0300

    Create route for home page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,31 +1,31 @@
-// import java.util.HashMap;
-// import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
-// import static spark.Spark.*;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
-// public class App {
-//   public static void main(String[] args) {
+public class App {
+  public static void main(String[] args) {
 
-//     ProcessBuilder process = new ProcessBuilder();
-//     Integer port;
-//     if (process.environment().get("PORT") != null) {
-//       port = Integer.parseInt(process.environment().get("PORT"));
-//     } else {
-//       port = 4567;
-//     }
+    ProcessBuilder process = new ProcessBuilder();
+    Integer port;
+    if (process.environment().get("PORT") != null) {
+      port = Integer.parseInt(process.environment().get("PORT"));
+    } else {
+      port = 4567;
+    }
 
-//     setPort(port);
+    setPort(port);
 
 
-//     staticFileLocation("/public");
-//     String layout = "templates/layout.vtl";
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
 
-//     get("/", (request, response) -> {
-//       HashMap<String, Object> model = new HashMap<String, Object>();
-//       model.put("stylists", Stylist.all());
-//       model.put("template", "templates/index.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("stylists", Stylist.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
 //     post("/stylists", (request, response) -> {
 //       HashMap<String, Object> model = new HashMap<String, Object>();

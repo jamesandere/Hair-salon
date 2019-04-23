aef50aa57ccf3ebad7153374ad5e1c26ce204328
jamesandere <jamesandere56@gmail.com>
2019-04-23 17:16:59 +0300

    Create function to save stylists

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,42 +1,42 @@
-// import java.util.List;
-// import org.sql2o.*;
+import java.util.List;
+import org.sql2o.*;
 
-// public class Stylist {
-//   private String name;
-//   private int id;
+public class Stylist {
+  private String name;
+  private int id;
 
 
-//   public Stylist(String name) {
-//     this.name = name;
-//   }
+  public Stylist(String name) {
+    this.name = name;
+  }
 
-//   public String getName() {
-//     return name;
-//   }
+  public String getName() {
+    return name;
+  }
 
-//   public int getId() {
-//     return id;
-//   }
+  public int getId() {
+    return id;
+  }
 
-//   @Override
-//   public boolean equals(Object otherStylist) {
-//     if(!(otherStylist instanceof Stylist)) {
-//       return false;
-//     }else {
-//       Stylist newStylist = (Stylist) otherStylist;
-//       return this.getName().equals(newStylist.getName());
-//     }
-//   }
+  @Override
+  public boolean equals(Object otherStylist) {
+    if(!(otherStylist instanceof Stylist)) {
+      return false;
+    }else {
+      Stylist newStylist = (Stylist) otherStylist;
+      return this.getName().equals(newStylist.getName());
+    }
+  }
 
-//   public void save() {
-//     try(Connection con = DB.sql2o.open()) {
-//       String sql = "INSERT INTO stylists(name) VALUES (:name)";
-//       this.id = (int) con.createQuery(sql, true)
-//       .addParameter("name", this.name)
-//       .executeUpdate()
-//       .getKey();
-//     }
-//   }
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO stylists(name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", this.name)
+      .executeUpdate()
+      .getKey();
+    }
+  }
 
 //   public static List<Stylist> all() {
 //     String sql = "SELECT * FROM stylists";

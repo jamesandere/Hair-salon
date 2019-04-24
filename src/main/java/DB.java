de28e2a69ea99b53ecc923beff20394410d56ee4
jamesandere <jamesandere56@gmail.com>
2019-04-24 21:41:52 +0300

    deploy

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -10,7 +10,7 @@ public class DB {
     private static URI dbUri;
     public static Sql2o sql2o;
     static {
-
+        
         try {
             if (System.getenv("DATABASE_URL") == null) {
                 dbUri = new URI("postgres://mrlegwtebcblxo:aa2719e251ec182861ea51579bfa77990d57aac60c1eff89507c960b81167b03@ec2-54-225-116-36.compute-1.amazonaws.com:5432/d7l4lmosh9gq76@localhost:5432/hair_salon_one");

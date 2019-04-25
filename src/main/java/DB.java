c415b5603f23a7a4d68526441a757857fd37a9c4
jamesandere <jamesandere56@gmail.com>
2019-04-25 07:25:42 +0300

    deploy

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -4,18 +4,19 @@
 //  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_one", "moringa", "moringa");
 // }
 import org.sql2o.*;
+import org.sql2o.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 public class DB {
     private static URI dbUri;
     public static Sql2o sql2o;
     static {
-        
+
         try {
             if (System.getenv("DATABASE_URL") == null) {
-                dbUri = new URI("postgres://mrlegwtebcblxo:aa2719e251ec182861ea51579bfa77990d57aac60c1eff89507c960b81167b03@ec2-54-225-116-36.compute-1.amazonaws.com:5432/d7l4lmosh9gq76@localhost:5432/hair_salon_one");
+                dbUri = new URI("postgres://moringa:moringa@localhost:5432/hair_salon_one");
             } else {
-                dbUri = new URI(System.getenv("DATABASE_URL")); 
+                dbUri = new URI(System.getenv("DATABASE_URL"));
             }
             int port = dbUri.getPort();
             String host = dbUri.getHost();
